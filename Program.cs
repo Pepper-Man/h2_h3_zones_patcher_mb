b64ac2b50d3fda981ab131cec93347a846f06df2
Jake Boraman <jake.boraman@gmail.com>
2023-08-06 21:38:22 +0100

    Initial commit, XML to parsed TXT functionality

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using Bungie;
+using System;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+
+class MB_Zones
+{
+    static void Main(string[] args)
+    {
+        // Variables
+        string h3ek_path = @"C:\Program Files (x86)\Steam\steamapps\common\H3EK";
+        string xml_path = @"G:\Steam\steamapps\common\H2EK\04a_gasgiant.xml";
+
+        ManagedBlamSystem.InitializeProject(InitializationType.TagsOnly, h3ek_path);
+        Convert_XML(xml_path);
+    }
+
+    static void Convert_XML(string xml_path)
+    {
+        Console.WriteLine("Beginning XML Conversion:\n\n");
+
+        XmlDocument scenfile = new XmlDocument();
+        scenfile.Load(xml_path);
+
+        XmlNode root = scenfile.DocumentElement;
+
+        XmlNodeList zones_start_blocks = root.SelectNodes(".//block[@name='zones']");
+
+        List<string> zones_list = new List<string>();
+
+        // Get all zone names
+        foreach (XmlNode zones_block in zones_start_blocks)
+        {
+            bool zones_end = false;
+            int i = 0;
+            while (!zones_end)
+            {
+                string search_string = "./element[@index='" + i + "']";
+                XmlNode element = zones_block.SelectSingleNode(search_string);
+
+                if (element != null)
+                {
+                    string zoneName = element.SelectSingleNode("./field[@name='name']").InnerText.Trim();
+                    zones_list.Add(zoneName);
+                    Console.WriteLine(zoneName);
+                    i++;
+                }
+                else
+                {
+                    zones_end = true;
+                }
+            }
+        }
+
+        // Temp file creation/wiping
+
+        using (FileStream fs = new FileStream("temp_output.txt", FileMode.Create, FileAccess.Write))
+        using (StreamWriter sw = new StreamWriter(fs))
+        {
+            // Get areas and firing positions for each name
+            int i = 0;
+            foreach (string zone_name in zones_list)
+            {
+                // Areas
+                sw.WriteLine("Zone: " + zone_name + "\nAreas:");
+                XmlNodeList areasBlockList = root.SelectNodes(".//block[@name='zones']/element[@index='" + i + "']/block[@name='areas']");
+                foreach (XmlNode area_block in areasBlockList)
+                {
+                    bool areas_end = false;
+                    int j = 0;
+                    while (areas_end == false)
+                    {
+                        string search_string = "./element[@index='" + j.ToString() + "']";
+                        var element = area_block.SelectSingleNode(search_string);
+
+                        if (element != null)
+                        {
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='name']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='area flags']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='runtime starting index']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='runtime count']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='manual reference frame']").InnerText.Trim());
+                            j++;
+                        }
+                        else
+                        {
+                            areas_end = true;
+                        }
+                    }
+                }
+
+                // Firing Positions
+                sw.WriteLine("Firing Positions:");
+                XmlNodeList fpos_block_list = root.SelectNodes(".//block[@name='zones']/element[@index='" + i + "']/block[@name='firing positions']");
+                foreach (XmlNode fpos_block in fpos_block_list)
+                {
+                    bool fpos_end = false;
+                    int j = 0;
+                    while (fpos_end == false)
+                    {
+                        string search_string = "./element[@index='" + j.ToString() + "']";
+                        var element = fpos_block.SelectSingleNode(search_string);
+
+                        if (element != null)
+                        {
+                            sw.WriteLine("index = " + j.ToString());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='position (local)']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='reference frame']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='flags']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./block_index[@name='short block index']").Attributes["index"].Value.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='cluster index']").InnerText.Trim());
+                            sw.WriteLine(element.SelectSingleNode("./field[@name='normal']").InnerText.Trim());
+                            j++;
+                        }
+                        else
+                        {
+                            fpos_end = true;
+                        }
+                    }
+                }
+                i++;
+            }
+        }
+    }
+}
