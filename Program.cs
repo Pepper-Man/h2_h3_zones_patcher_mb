560cfdece60a00ca47e0878f8f8838ce5cad467f
Jake Boraman <jake.boraman@gmail.com>
2023-08-07 11:03:40 +0100

    Converted code for data processing

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Xml;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 class MB_Zones
 {
@@ -122,5 +123,182 @@ class MB_Zones
                 i++;
             }
         }
+        Edit_The_Tags();
+    }
+
+    static void Edit_The_Tags()
+    {
+        Console.WriteLine("Beginning tag writing process...");
+
+        int zone = -1;
+        bool areas = false;
+        bool fpos = false;
+        int areaIndex = -1;
+        int fposIndex = -1;
+        int areaDataCount = 0;
+        int fposDataCount = 0;
+        int fposFlagLineSkip = 0;
+        bool previousWasActualFlag = false;
+
+        string[] text = File.ReadAllLines("temp_output.txt");
+
+        foreach (string line in text)
+        {
+            if (line.Contains("Zone:"))
+            {
+                zone++;
+                Console.WriteLine(line.Trim());
+                continue;
+            }
+            else if (line.Contains("Areas:"))
+            {
+                areas = true;
+                fpos = false;
+                areaIndex = 0;
+                areaDataCount = 0;
+                continue;
+            }
+            else if (line.Contains("Firing Positions:"))
+            {
+                fpos = true;
+                areas = false;
+                fposDataCount = 0;
+                fposIndex = 0;
+                continue;
+            }
+            else if (areas)
+            {
+                if (previousWasActualFlag)
+                {
+                    previousWasActualFlag = false;
+                    continue;
+                }
+                if (areaDataCount == 0)
+                {
+                    // Patch area name
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].areas[{areaIndex}].name";
+                    Console.WriteLine($"patching {line.Trim()}");
+                    PatchTag(fieldPath, line.Trim());
+                    areaDataCount++;
+                }
+                else if (areaDataCount == 1)
+                {
+                    // Patch area flags
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].areas[{areaIndex}].area flags";
+                    Console.WriteLine("area flags");
+                    PatchTag(fieldPath, line.Trim());
+                    areaDataCount++;
+                    if (line.Trim() != "0")
+                    {
+                        previousWasActualFlag = true;
+                    }
+                }
+                else if (areaDataCount == 2)
+                {
+                    // line is runtime starting index
+                    areaDataCount++;
+                    continue;
+                }
+                else if (areaDataCount == 3)
+                {
+                    // line is runtime count
+                    areaDataCount++;
+                    continue;
+                }
+                else if (areaDataCount == 4)
+                {
+                    // Patch manual reference frame
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].areas[{areaIndex}].manual reference frame";
+                    Console.WriteLine($"manual ref frame = {line.Trim()}");
+                    PatchTag(fieldPath, ",-1");
+                    areaDataCount = 0;
+                    areaIndex++;
+                }
+            }
+            else if (fpos)
+            {
+                if (fposFlagLineSkip > 0)
+                {
+                    fposFlagLineSkip--;
+                    continue;
+                }
+                if (fposDataCount == 0)
+                {
+                    // Patch firing position index
+                    Console.WriteLine($"firing position {line.Trim()} patch start");
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 1)
+                {
+                    // Patch firing position position
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].position (local)";
+                    Console.WriteLine("patching position");
+                    PatchTag(fieldPath, line.Trim());
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 2)
+                {
+                    // Patch firing position ref frame
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].reference frame";
+                    Console.WriteLine("patching ref frame");
+                    PatchTag(fieldPath, ",-1");
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 3)
+                {
+                    // Patch firing position flags
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].flags";
+                    Console.WriteLine("patching flags");
+                    PatchTag(fieldPath, Regex.Replace(line.Trim(), "[^0-9]", ""));
+                    if (int.Parse(line.Trim()) > 75)
+                    {
+                        fposFlagLineSkip = 3;
+                    }
+                    else if (int.Parse(line.Trim()) < 10 && int.Parse(line.Trim()) >= 1)
+                    {
+                        fposFlagLineSkip = 1;
+                    }
+                    else if (int.Parse(line.Trim()) == 0)
+                    {
+                        fposFlagLineSkip = 0;
+                    }
+                    else
+                    {
+                        fposFlagLineSkip = 2;
+                    }
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 4)
+                {
+                    // Patch firing position area
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].area";
+                    Console.WriteLine("patching area");
+                    PatchTag(fieldPath, "," + line.Trim());
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 5)
+                {
+                    // Patch firing position cluster index
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].cluster index";
+                    Console.WriteLine("patching cluster index");
+                    PatchTag(fieldPath, line.Trim());
+                    fposDataCount++;
+                }
+                else if (fposDataCount == 6)
+                {
+                    // Patch firing position normal
+                    string fieldPath = $"scenario_struct_definition[0].zones[{zone}].firing positions[{fposIndex}].normal";
+                    Console.WriteLine("patching normal");
+                    PatchTag(fieldPath, line.TrimEnd('\n'));
+                    fposDataCount = 0;
+                    fposIndex++;
+                }
+            }
+        }
+    }
+
+    static void PatchTag(string fieldPath, string line)
+    {
+        Console.WriteLine("Patching " + line);
     }
 }
